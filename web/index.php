321a575691f2a5e5c993a490c1e56aebb1a011a2
Aleksander Promvalds <aleksander.promvalds@khk.ee>
2018-09-16 22:31:34 +0300

    Install Doctrine DBAL with Composer, Update documentation

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
  $app = new Silex\Application();
- $app['debug'] = true;
+# $app['debug'] = true;
  
  $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../views',
